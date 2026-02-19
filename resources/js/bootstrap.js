050555ba73b669082d38d048a1f16cb322559415
JhonrBermudez26 <rdeivis260502@gmail.com>
2026-02-19 18:24:11 -0500

    configuracion completa

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,12 +8,12 @@ import Pusher from "pusher-js";
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
-    broadcaster: "reverb",
+     broadcaster: "reverb",
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT || 8080,
-    wssPort: import.meta.env.VITE_REVERB_PORT || 8080,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME || "http") === "https",
+    wsPort: import.meta.env.VITE_REVERB_PORT,
+    wssPort: import.meta.env.VITE_REVERB_PORT,
+    forceTLS: import.meta.env.VITE_REVERB_SCHEME === "https", // true solo si usas https
     enabledTransports: ["ws", "wss"],
     disableStats: true,
 });
